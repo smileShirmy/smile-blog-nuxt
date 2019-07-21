8f30845a08f42606936dd2d4976b08307340a108
smileShirmy <qiushiming1997@gmail.com>
2019-07-21 15:06:17 +0800

    update: 完善代码

--- plugins/copy.js
+++ plugins/copy.js
@@ -1,29 +1,19 @@
 if (process.env.NODE_ENV === 'production') {
-  const copyText = () => {
-    return `
+  const copyText = `
 
 ---------------------
 作者：shirmy
 链接：${location.href}
-来源：https://shirmy.me
+来源：https://www.shirmy.me
 商业转载请联系作者获得授权，非商业转载请注明出处。`
-  }
-
-  const buildText = content => {
-    return content + copyText()
-  }
-
-  const buildHtml = content => {
-    return content + copyText()
-  }
 
   document.addEventListener('copy', e => {
     if (!window.getSelection) {
       return
     }
     const content = window.getSelection().toString()
-    e.clipboardData.setData('text/plain', buildText(content))
-    e.clipboardData.setData('text/html', buildHtml(content))
+    e.clipboardData.setData('text/plain', content + copyText)
+    e.clipboardData.setData('text/html', content + copyText)
     e.preventDefault()
   })
 }
