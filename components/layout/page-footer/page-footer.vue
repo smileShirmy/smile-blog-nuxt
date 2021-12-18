b19cf5ad162312a3e3645c2996b821ad88045f29
Shirmy <qiushiming1997@gmail.com>
2021-12-18 19:05:45 +0800

    feat: 备案号修改

--- components/layout/page-footer/page-footer.vue
+++ components/layout/page-footer/page-footer.vue
@@ -22,7 +22,7 @@
     </div>
     <div class="footer-copyright">
       <span class="copyright">© 2019 <span class="logo">Smile</span></span>. All Right Reserved.
-      <a class="record-number" target="_blank" href="https://beian.miit.gov.cn/">粤ICP备17101737号-1</a>
+      <a class="record-number" target="_blank" href="https://beian.miit.gov.cn/">粤ICP备17101737号</a>
     </div>
   </footer>
 </template>
